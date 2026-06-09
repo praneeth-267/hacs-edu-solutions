a91c36386b3ff18da6e949ff39a82309428686c1
praneeth-267 <praneethharsha12@gmail.com>
2026-06-09 02:13:53 +0200

    update vit config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/hacs-website-frontend/",
+  base: "/hacs-edu-solutions/",
   plugins: [react()],
   server: {
     port: 5173,
